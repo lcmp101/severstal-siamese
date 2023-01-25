0e82477c6348b8d129c788189af45d5cafa49a1d
lcmp101 <36266493+lcmp101@users.noreply.github.com>
2023-01-25 10:13:21 +0100

    Update trainpt.py

--- trainpt.py
+++ trainpt.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
 from siamese import SiameseNetwork
-from libs.dataset import Dataset
+from functions.dataset import Dataset
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
